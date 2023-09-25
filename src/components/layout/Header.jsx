3bad19167d5f275082126beef81c880e8621c825
alegandolfo <alegandolfo10@gmail.com>
2023-09-25 19:31:15 -0300

    feat: add header component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header id='header'>
+        <p>Sistema Digital de Proteção ao Cidadão</p>
+    </header>
+  )
+}
+
+export default Header
