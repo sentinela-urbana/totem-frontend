cbd1e4710b34b71f0ee11af28716b45a7c2e81a9
alegandolfo <alegandolfo10@gmail.com>
2023-10-16 20:22:03 -0300

    chore: add CameraModal component

--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -1,8 +1,8 @@
 import './Button.css'
 
-const Button = ({title}) => {
+const Button = ({title, variant}) => {
   return (
-    <button>{title}</button>
+    <button className={variant}>{title}</button>
   )
 }
 
---
+++ src/components/utils/CameraModal.jsx
@@ -0,0 +1,25 @@
+import Button from './Button'
+import './CameraModal.css'
+
+const CameraModal = ({video, address, neighborhood, referencePoint, cep}) => {
+  return (
+    <div className='cameraModal'>
+      <main>
+        <iframe className='cameraModalVideo' src={video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <div className='modalTextWrapper'>
+          <div className='modalLocations'>
+            <p className='modalAdress'>{address}</p>
+            <p className='modalReferece'>{neighborhood} - {referencePoint}</p>
+          </div>
+          <p className='modalCEP'>CEP: {cep}</p>
+        </div>
+        <div className='buttonWrapper'>
+          <Button title={'Liberar Chamada'} variant={'modalBlue'}/>
+          <Button title={'Fechar'} variant={'modalGray'}/>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default CameraModal
