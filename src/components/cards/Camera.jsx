055ad549f872f0a4e5faa2892ea091351093c498
alegandolfo <alegandolfo10@gmail.com>
2023-10-16 20:38:43 -0300

    refactor: add neighborhood to camera text format

--- src/components/cards/Camera.jsx
+++ src/components/cards/Camera.jsx
@@ -1,10 +1,10 @@
 import './Camera.css'
 
-const Camera = ({video}) => {
+const Camera = ({video, address, neighborhood}) => {
   return (
     <div className='camera'>
       <iframe className='cameraVideo' src={video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <p className='cameraText'>Av. José da Costa Pinheiro Júnior, 1970 - Travessão</p>
+      <p className='cameraText'>{address} - {neighborhood}</p>
     </div>
   )
 }
