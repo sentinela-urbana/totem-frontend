a7fe628f6c859c248f49794a4dac2c99e870d113
alegandolfo <alegandolfo10@gmail.com>
2023-11-06 21:03:57 -0300

    fix: compile svg image file

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,29 @@
 import './Header.css'
 import profileIcon from '../../../public/profileIcon.svg'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Header = () => {
+const Header = ({isLoggedIn}) => {
+
+  if (!isLoggedIn)
+  return (
+    <header id='header'>
+      <p>Sistema Digital de Proteção ao Cidadão</p>
+    </header>
+  )
+
   return (
     <header id='header'>
       <p>Sistema Digital de Proteção ao Cidadão</p>
 
       <div id='userSect'>
-        <img src={profileIcon} alt='userPic'/>
+        <Image src={profileIcon} alt='userPic'/>
         <div id='labels'>
           <p>Usuario</p>
-          <p style={{cursor: 'pointer'}}>Sair</p>
+          <Link style={{textDecoration: 'none'}} href={'/login'}>
+            <p style={{cursor: 'pointer', color: '#FFF'}}>Sair</p>
+          </Link>
         </div>
-
       </div>
     </header>
   )
